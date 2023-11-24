27910370156cbcd6060790193873946eb0bc0242
soledad <soledad@smartescrow.es>
2023-11-24 15:09:55 +0100

    AdjustFunctionDB

--- conection_LocalDatabase.py
+++ conection_LocalDatabase.py
@@ -1,4 +1,3 @@
-"""
 from pymongo import MongoClient
 import os
 from datetime import datetime
@@ -10,6 +9,7 @@ mongo_port = "27017"
 database_name = "db_borme_empresas"
 DATABASE_URI = f"mongodb://{mongo_host}:{mongo_port}/{database_name}"
 
+# ---------------------------------------------------- CONEXION A MONGO --------------------------------------------
 def dbConnection():
     try:
         client = MongoClient(DATABASE_URI)
@@ -19,6 +19,10 @@ def dbConnection():
     except Exception as e:
         print(f'Error al conectar a la DB: {e}')
         return None
+# --------------------------------------------------VERIFICAR E INSERTAR COMPANY ---------------------------------------------------------
+# Verifica si una compañía ya existe en la base de datos.
+#Si la compañía existe, comprueba si hay nuevas inscripciones que agregar.
+#Si la compañía no existe, la inserta en la base de datos.
 
 def verificar_e_insertar_compania(db, company):
     # Buscar si la compañía ya existe en la base de datos
@@ -44,26 +48,58 @@ def verificar_e_insertar_compania(db, company):
         # La compañía no existe, insertarla nueva
         result = db['company'].insert_one(company)
         print(f"Nueva compañía insertada, ID: {result.inserted_id}")
-        
-def guardar_registro_archivo(nombre_archivo, ruta_archivo_registros, es_error=False):
-    estado = "ERROR" if es_error else "PROCESADO"
-    with open(ruta_archivo_registros, 'a') as archivo:
-        archivo.write(f"{nombre_archivo} - {estado}\n")        
 
-def procesar_pdf(db, ruta_archivo_registros, pdf_path):
-    print(f"Procesando archivo: {pdf_path}")
+# ------------------------------------------- GUARDAR EN UN TXT LOS BORME PROCESADOS Y CON ERRORES  -----------------------
+# Guardo en un txt el ultimo archivo procesados correctamente y en otro la lista de todos los archivos con error
+       
+def guardar_registro_archivo(nombre_archivo, fecha_archivo, ruta_archivo_registros, es_error=False):
+    # Sobrescribe el archivo con el nombre del último archivo procesado correctamente
+    with open(ruta_archivo_registros, 'w') as archivo:
+        archivo.write(f"{nombre_archivo} - {fecha_archivo} - PROCESADO\n")  
+
+def guardar_registro_error(nombre_archivo, fecha_archivo, ruta_archivo_registros):
+    # Agrega el nombre de todos los archivos con error a un archivo de texto separado
+    with open(ruta_archivo_registros, 'a') as archivo:
+        archivo.write(f"{nombre_archivo} - {fecha_archivo} - ERROR\n")
+       
+
+# ---------------------------------------------------- PROCESAMIENTO PDF  -------------------------------------------- 
+#Procesa un archivo PDF individual.
+#Extrae los datos del archivo PDF y los inserta en la base de datos.
+#Registra el archivo como procesado o con error en el archivo de registro.
+
+def procesar_pdf(db, ruta_archivo_registros, pdf_path, archivos_procesados=None):
+    nombre_archivo = os.path.basename(pdf_path)
+
+    # Extraer la fecha del path del archivo
+    partes_path = pdf_path.split('/')
+    # Seleccionar las partes que corresponden a año, mes y día
+    if len(partes_path) > 3:
+        fecha_archivo = f"{partes_path[-5]}-{partes_path[-4]}-{partes_path[-3]}"
+        print(fecha_archivo)
+    else:
+        fecha_archivo = "Fecha desconocida"
+
+    if archivos_procesados and nombre_archivo in archivos_procesados:
+        print(f"Archivo ya procesado: {nombre_archivo}")
+        return
+        
+    print(f"Procesando archivo: {pdf_path} (Fecha: {fecha_archivo})")
     try:
         data_to_insert = file_type_a(pdf_path)
         if isinstance(data_to_insert, list):
             for company in data_to_insert:
                 verificar_e_insertar_compania(db, company)
         # Guardar el registro del archivo procesado
-        guardar_registro_archivo(os.path.basename(pdf_path), ruta_archivo_registros)
+        guardar_registro_archivo(nombre_archivo, fecha_archivo, ruta_archivo_registros)
     except Exception as e:
-        print(f"Error al procesar el archivo {os.path.basename(pdf_path)}: {e}")
+        print(f"Error al procesar el archivo {pdf_path} (Fecha: {fecha_archivo})")
         # Guardar el registro del archivo con error
-        guardar_registro_archivo(os.path.basename(pdf_path), ruta_archivo_registros, es_error=True)
+        guardar_registro_error(nombre_archivo, fecha_archivo, ruta_archivo_registros)
         
+ # ----------------------------------------------------   --------------------------------------------  
+ #Carga un conjunto de nombres de archivos que ya han sido procesados a partir de un archivo de texto.
+ #Esto evita el reprocesamiento de archivos que ya han sido manejados.      
 def cargar_archivos_procesados(ruta_archivo_registros):
     if not os.path.exists(ruta_archivo_registros):
         return set()
@@ -73,13 +109,27 @@ def cargar_archivos_procesados(ruta_archivo_registros):
         archivos_procesados = set(linea.split(' - ')[0].strip() for linea in lineas)
     return archivos_procesados        
 
+# ----------------------------------------------------  -------------------------------------------- 
+# Similar a la primera versión, pero también verifica si un archivo ya ha sido procesado antes de intentar procesarlo.
+
 def procesar_pdf(db, ruta_archivo_registros, pdf_path, archivos_procesados):
     nombre_archivo = os.path.basename(pdf_path)
-    if nombre_archivo in archivos_procesados:
-        print(f"Archivo ya procesado: {nombre_archivo}")
-        return
+    print (nombre_archivo)
+    print (pdf_path)
+    # Extraer la fecha del path del archivo
+    partes_path = pdf_path.split('/')
+    # Seleccionar las partes que corresponden a año, mes y día
+    if len(partes_path) > 3:
+        fecha_archivo = f"{partes_path[-5]}-{partes_path[-4]}-{partes_path[-3]}"
+        print(fecha_archivo)    
+    else:
+        fecha_archivo = "Fecha desconocida"
 
-    print(f"Procesando archivo: {pdf_path}")
+    if archivos_procesados and nombre_archivo in archivos_procesados:
+        print(f"Archivo ya procesado: {nombre_archivo} (Fecha: {fecha_archivo})")
+        return
+        
+    print(f"Procesando archivo: {pdf_path} (Fecha: {fecha_archivo})")
     try:
         data_to_insert = file_type_a(pdf_path)
         if isinstance(data_to_insert, list):
@@ -93,10 +143,14 @@ def procesar_pdf(db, ruta_archivo_registros, pdf_path, archivos_procesados):
                     inscription['createdAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                     inscription['updatedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 verificar_e_insertar_compania(db, company)
-        guardar_registro_archivo(nombre_archivo, ruta_archivo_registros)
+        guardar_registro_archivo(nombre_archivo, fecha_archivo, ruta_archivo_registros)
     except Exception as e:
-        print(f"Error al procesar el archivo {nombre_archivo}: {e}")
-        guardar_registro_archivo(nombre_archivo, ruta_archivo_registros, es_error=True)
+        print(f"Error al procesar el archivo {nombre_archivo} (Fecha: {fecha_archivo}): {e}")
+        guardar_registro_error(nombre_archivo, fecha_archivo, ruta_archivo_registros)
+
+# ----------------------------------------------------   -------------------------------------------- 
+# Procesa archivos PDF en un directorio específico, ordenándolos por año, mes y día.
+# Maneja archivos de tipo 'BORME-A', 'BORME-B', 'BORME-C' y 'BORME-S', aunque actualmente solo procesa archivos 'BORME-A'.
 
 def procesar_pdfs_por_orden_y_tipo(directory, db, ruta_archivo_registros, archivos_procesados):
     for year in sorted(os.listdir(directory)):
@@ -116,11 +170,13 @@ def procesar_pdfs_por_orden_y_tipo(directory, db, ruta_archivo_registros, archiv
                                 if archivo.startswith(('BORME-B', 'BORME-C', 'BORME-S')) and not archivo.startswith('BORME-A'):
                                     procesar_pdf(db, ruta_archivo_registros, os.path.join(path_day, archivo), archivos_procesados)
 
+
+
+
 if __name__ == '__main__':
-    directory = '/home/soledad/BD.BORME-Empresas/files/prueba'
+    directory = '/home/soledad/BD.BORME-Empresas/files/pruebas chicas/prueba_A'
     db = dbConnection()
     ruta_archivo_registros = '/home/soledad/BD.BORME-Empresas/files/registro_archivos_prueba.txt'
     archivos_procesados = cargar_archivos_procesados(ruta_archivo_registros)
 
     procesar_pdfs_por_orden_y_tipo(directory, db, ruta_archivo_registros, archivos_procesados)
-"""
