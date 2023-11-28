897c33c28b49fd9ed491a606c48e5f0eebb67312
soledad <soledad@smartescrow.es>
2023-11-28 17:26:05 +0100

    addFieldsBormeB

--- Files_B.py
+++ Files_B.py
@@ -1,43 +1,286 @@
-"""
-public function handleFileTypeB($textoDelPdf, $fileName)
-    {
-        $inscription = '';
+from flask import Flask, jsonify
+import pdfplumber
+import re
+import os
+from datetime import datetime
 
-        if (preg_match('/^Otros actos publicados en el Registro Mercantil\n([A-Z\/ÁÉÍÓÚÜ]+)\n/m', $textoDelPdf, $matches)) {
-            $comercialRegistryConstitucion = $matches[1];
-        }
+
+app = Flask(__name__)
+
+# Defino la función para extraer texto
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+
+# Función para extraer texto en negrita del PDF
+def extract_bold_text(pdf_path):
+    bold_text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            for char in page.chars:
+                if 'Bold' in char['fontname']:
+                    bold_text += char['text']
+    return bold_text
+
+def find_bold_for_inscription(inscription, bold_text):
+    # Extraer el número de inscripción
+    inscription_number_match = re.search(r'^(\d+)', inscription)
+    if not inscription_number_match:
+        return "No disponible"
+
+    inscription_number = inscription_number_match.group(1)
+
+    # Buscar el patrón en el texto en negrita
+    pattern = fr'{inscription_number} - [^\.]+?\.(.*?)(?=\d+ - [^\.]+?\.|$)'
+    bold_match = re.search(pattern, bold_text)
+
+    if bold_match:
+        # Limpiar el texto capturado para eliminar cualquier texto no deseado al final
+        bold_text_captured = bold_match.group(1).strip()
+        # Eliminar cualquier texto no relacionado que pueda haberse capturado al final
+        clean_text = re.sub(r'BOLETÍN OFICIAL DEL REGISTRO MERCANTIL.*', '', bold_text_captured)
+        return clean_text
+    else:
+        return "No disponible"
+    
+ 
+# -------------------------------------------------- PROCESAMIENTO BORME A --------------------------------------------------------------
+
+def file_type_a(pdf_path):
+    companies = []
+    
+    texto_del_pdf = extract_text_from_pdf(pdf_path)
+    bold_text = extract_bold_text(pdf_path)
+    
+    
+    # Extraer la fecha del Borme
+    borme_date_pattern = r'(Lunes|Martes|Miércoles|Jueves|Viernes|Sábado|Domingo)\s+(\d+)\s+de\s+(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)\s+de\s+(\d{4})'
+    borme_date_match = re.search(borme_date_pattern, texto_del_pdf)
+    
+    if borme_date_match:
+    # Extraemos el día, mes (en texto) y año
+     day, month_text, year = borme_date_match.groups()[1:]
+    # Convertimos el mes a su correspondiente número
+     months = {"enero": "01", "febrero": "02", "marzo": "03", "abril": "04",
+              "mayo": "05", "junio": "06", "julio": "07", "agosto": "08",
+              "septiembre": "09", "octubre": "10", "noviembre": "11", "diciembre": "12"}
+     month = months[month_text.lower()]
+     
+    # Formatear la fecha al formato DD/MM/YYYY
+     borme_date = f"{day.zfill(2)}/{month}/{year}"
+    else:
+     borme_date = "Fecha no encontrada"
+     
+     
+     
+    # Buscar la provincia del Acto
+    inscription_commercial_registry_pattern = r'^Actos inscritos\n([A-Z\/ÁÉÍÓÚÜ\s]+)\n'
+    inscription_commercial_registry_match = re.search(inscription_commercial_registry_pattern, texto_del_pdf, re.MULTILINE)
+    inscription_commercial_registry = inscription_commercial_registry_match.group(1) if inscription_commercial_registry_match else "No encontrado"
+
+    # Buscar Sección del Acto
+    inscription_section_pattern = r'Pág\.\s+\d+\s+SECCIÓN\s+(PRIMERA|SEGUNDA)'
+    inscription_section_match = re.search(inscription_section_pattern, texto_del_pdf)
+    inscription_section = inscription_section_match.group(1) if inscription_section_match else "No encontrado"
+
+    # Buscar Categoria del Acto
+    inscription_category_pattern = r'Empresarios\s+(.*)\s+'
+    inscription_category_match = re.search(inscription_category_pattern, texto_del_pdf)
+    inscription_category = inscription_category_match.group(1) if inscription_category_match else "No encontrado"
+
+    # Buscar todos los actos inscritos y extraer informacion
+    inscription_pattern = r'^(\d+ - .+?\(\s*\d{1,2}\.\d{1,2}\.\d{2,4}\s*\)\.)'
+    inscriptions = re.findall(inscription_pattern, texto_del_pdf, re.MULTILINE | re.DOTALL)
+
+    for inscription in inscriptions:
+    # Expresión regular para buscar el número de inscripción
+        inscription_number_pattern = r'^(\d+)'
+        number_match = re.search(inscription_number_pattern, inscription)
+        if number_match:
+         inscription_number = number_match.group(1)
+        else:
+         inscription_number = 'Número no encontrado'
+
+    # Expresión regular para buscar la denominación social completa
+        full_name_pattern = r'^\d+ - (.*?)(?:\.\n|$)'
+        full_name_match = re.search(full_name_pattern, inscription, re.IGNORECASE)
+    
+        if full_name_match:
+         company_social_denomination = full_name_match.group(1).strip()
+        else:
+         company_social_denomination = 'Denominación no encontrada'
+    # Expresión regular para buscar solo el nombre de la empresa (sin SL, Sociedad Limitada, etc.)
+        name_pattern = r'^\d+ - (.*?)(?:,? (SOCIEDAD LIMITADA|SL|SLL|SL EN LIQUIDACION|SLNE|SLNE EN LIQUIDACION|SA|SA EN LIQUIDACION|SOCIEDAD LIMITADA LABORAL|SOCIEDAD ANONIMA|S\.L\.|S\.L\.L\.|S\.A\.|S\.A\.A\.|S\.L\.L\.))?\.\n'
+        name_match = re.search(name_pattern, inscription, re.IGNORECASE)
+    
+        if name_match:
+         company_name = name_match.group(1).strip()
+        else:
+         company_name = 'Nombre no encontrado' 
+            
+    # Expresión regular para extraer los datos registrales
+        registry_data_pattern = r'Datos\s*registrales\.(.*?)\s*\('
+        registry_data_match = re.search(registry_data_pattern, inscription, re.DOTALL)
+    
+        if registry_data_match:
+         inscription_registry_data = " ".join(registry_data_match.group(1).split())
+        else:
+         inscription_registry_data = 'Datos registrales no encontrados'
+
+    # Expresión regular para extraer la fecha
+        date_pattern = r'\(\s*(\d{1,2})\.(\d{1,2})\.(\d{2,4})\)'
+        date_match = re.search(date_pattern, inscription)
+
+        if date_match:
+         day, month, year = date_match.groups()
+    # Asegurar que el día y el mes tengan dos dígitos
+         day = day.zfill(2)
+         month = month.zfill(2)
+
+    # Ajustar el año para que tenga cuatro dígitos
+         year = "20" + year if len(year) == 2 else year
+
+    # Formatear la fecha al formato DD/MM/YYYY
+         inscription_date = f"{day}/{month}/{year}"
+        else:
+          inscription_date = 'Fecha no encontrada' 
+            
+    # Relaciona cada acto con las palabras en negrita de inscription        
+        inscription_name = find_bold_for_inscription(inscription, bold_text)
         
-        $this->findBormeVars_A_B($textoDelPdf, $inscriptionCategory, $inscriptions);
-        $this->findGenericBormeSectionAndDate($textoDelPdf, $inscriptionSection ,$bormeDate);
+#--------------------------OBTENER LOS DATOS DE CONSTITUCION DE LA COMPANY -------------------------------------------------------------------------        
+    # Verificar si la inscripción contiene 'Constitución'
+        is_constitution = 'Constitución' in inscription
 
-        foreach ($inscriptions ?? [] as $key => $inscription) {
-            $pattern = "/^\d+\s-\s(.*)(\R|$)/m";
-            if (preg_match($pattern, $inscription, $matches)) {
-                $companyName = $matches[1];
-            } else {
-                $companyName = 'Nombre no encontrado';
-            }
+        # Extracción de datos específicos de constitución si es necesario
+        constitution_date, constitution_registry_data, constitution_inscription, operation_start_date, constitution_address, constitution_social_object, constitution_social_capital = (None,)*7
+        if is_constitution:
+    # Extraer la fecha de inicio de operaciones
+         operation_start_date_pattern = r'Comienzo de operaciones:\s*(\d{1,2}\.\d{1,2}\.\d{2,4})\.'
+         operation_start_date_match = re.search(operation_start_date_pattern, inscription)
+         operation_start_date = operation_start_date_match.group(1) if operation_start_date_match else "No disponible"
+             
 
-            if (preg_match("/^\d+/m", $inscription, $matches)) {
-                $inscriptionNumber = $matches[0];
-            }            
-            
-            $pattern = "/^(Depósito|Cierre|Reapertura|Cancelación).*/m";
-            if (preg_match($pattern, $inscription, $matches)) {
-                $inscriptionName = $matches[0] ?? 'ERROR';
-            }
-            
-            $this->persistCompanyInscription(
-                $inscriptionName ?? null,
-                $companyName ?? null,
-                $comercialRegistryConstitucion ?? null,
-                $inscriptionNumber ?? null,
-                $inscriptionSection ?? null,
-                $inscriptionCategory ?? null,
-                $inscription ?? null,
-                $fileName ?? null,
-                $bormeDate ?? null,
-            );
+    # Extraer el objeto social
+        constitution_social_object_pattern = r'Objeto social:\s*(.+?)\.\s*Domicilio:'
+        constitution_social_object_match = re.search(constitution_social_object_pattern, inscription, re.DOTALL)
+        constitution_social_object = constitution_social_object_match.group(1).strip() if constitution_social_object_match else "No disponible"
+
+    # Extraer el domicilio
+        constitution_address_pattern = r'Domicilio:\s*(.+?)\.\s*Capital:'
+        constitution_address_match = re.search(constitution_address_pattern, inscription, re.DOTALL)
+        constitution_address = constitution_address_match.group(1).replace('\n', ' ').strip() if constitution_address_match else "No disponible"
+
+    # Extraer el capital
+        constitution_social_capital_pattern = r'Capital:\s*([\d.,]+)\s*Euros\.'
+        constitution_social_capital_match = re.search(constitution_social_capital_pattern, inscription, re.DOTALL)
+        constitution_social_capital = constitution_social_capital_match.group(1).strip() if constitution_social_capital_match else "No disponible"
+        
+    # Expresión regular para extraer la fecha
+        date_pattern = r'\(\s*(\d{1,2})\.(\d{1,2})\.(\d{2,4})\)'
+        date_match = re.search(date_pattern, inscription)
+
+        if date_match:
+         day, month, year = date_match.groups()
+    # Asegurar que el día y el mes tengan dos dígitos
+         day = day.zfill(2)
+         month = month.zfill(2)
+
+    # Ajustar el año para que tenga cuatro dígitos
+         year = "20" + year if len(year) == 2 else year
+
+    # Formatear la fecha al formato DD/MM/YYYY
+         constitution_date = f"{day}/{month}/{year}"
+        else:
+          constitution_date = 'Fecha no encontrada' 
+          
+    # Expresión regular para extraer los datos registrales
+        registry_data_pattern = r'Datos\s*registrales\.(.*?)\s*\('
+        registry_data_match = re.search(registry_data_pattern, inscription, re.DOTALL)
+    
+        if registry_data_match:
+         constitution_registry_data = " ".join(registry_data_match.group(1).split())
+        else:
+         constitution_registry_data = 'Datos registrales no encontrados'  
+         
+         # Asignar los campos relacionados con la constitución
+        constitution_inscription = inscription
+        constitution_file = os.path.basename(pdf_path)
+        
+        
+        # Buscar la provincia del Acto
+        constitution_commercial_registry_pattern = r'^Actos inscritos\n([A-Z\/ÁÉÍÓÚÜ\s]+)\n'
+        constitution_commercial_registry_match = re.search(constitution_commercial_registry_pattern, texto_del_pdf, re.MULTILINE)
+        constitution_commercial_registry = constitution_commercial_registry_match.group(1) if constitution_commercial_registry_match else "No encontrado"
+       
+        company = {
+            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "companySocialDenomination": company_social_denomination,          
+            "companyName": company_name,
+            "companyNif": None,
+            "companyCurrentAddress": None,
+            "companyCurrentSocialObject": None,
+            "companyCurrentSocialCapital": None,
+            "companyPhoneNumber": None,
+            "companyEmail": None,
+            "companyWeb": None,
+            "companyEmployeesNumber": None,
+            "companyParent": None,
+            "constitutionCommercialRegistry": constitution_commercial_registry if is_constitution else "No disponible",
+            "constitutionDate": constitution_date if is_constitution else "No disponible",
+            "constitutionRegistryData": constitution_registry_data if is_constitution else "No disponible",
+            "constitutionInscription": constitution_inscription if is_constitution else "No disponible",
+            "operationsStartDate": operation_start_date if is_constitution else "No disponible",
+            "companyDuration": None,
+            "constitutionAddress": constitution_address if is_constitution else "No disponible",
+            "constitutionSocialObject": constitution_social_object if is_constitution else "No disponible",
+            "constitutionSocialCapital": constitution_social_capital if is_constitution else "No disponible",
+            "constitutionFile": constitution_file if is_constitution else "No disponible",            
+            "companyState": None,
+            "administration": None,
+            "administratorsList": None,
+            "administrationAppointmentDate": None,
+            "administrationAppointmentInscription": None,
+            "administrationAppointmentFile": None,
+            "companyInscription": []
         }
-    }
-    """
+        # Crear y agregar la inscripción a la empresa correspondiente
+        company_inscription  = {
+                "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "inscriptionCommercialRegistry": inscription_commercial_registry,
+                "inscriptionNumber": inscription_number,
+                "inscriptionSection": inscription_section,
+                "inscriptionCategory": inscription_category,
+                "inscriptionName": inscription_name,
+                "inscriptionDate": inscription_date,
+                "inscriptionRegistryData": inscription_registry_data,
+                "inscription": inscription,
+                "bormeDate": borme_date,
+                "inscriptionFile": os.path.basename(pdf_path)
+                }
+            
+        company["companyInscription"].append(company_inscription)
+        companies.append(company)
+
+    return companies
+
+#"""
+@app.route('/')  # Defino la ruta
+def home():
+    pdf_path = "files/prueba_A/2010/11/02/pdfs/BORME-A-2010-210-13.pdf"
+    #texto_del_pdf = extract_text_from_pdf(pdf_path)
+    #boldWords = extract_bold_text(pdf_path)
+    company = file_type_a(pdf_path)
+    
+    return jsonify(company)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+#"""
--- main.py
+++ main.py
@@ -36,15 +36,60 @@ def extract_italic_titles(pdf_path):
         titles.append(current_title.strip())
 
     return titles
- 
+
+def extract_blocks_with_titles(texto_del_pdf, pdf_path):    
+    italic_titles = extract_italic_titles(pdf_path)
+    blocks = []
+    current_block = ''
+    for line in texto_del_pdf.split('\n'):
+        if line in italic_titles:
+            if current_block:
+                blocks.append(current_block.strip())
+                current_block = ''
+            current_block += line + '\n'
+        else:
+            current_block += line + '\n'
+    if current_block:
+        blocks.append(current_block.strip())
+    return blocks
+
+def process_block(block):
+    lines = block.split('\n')
+    inscription_name = lines[0]  # El nombre de la inscripción es la primera línea del bloque
+    extracted_data = []
+        
+    for line in lines[1:]:
+        if line.strip() and '-' in line:
+            parts = line.split('-', 1)
+            if len(parts) == 2:
+                inscription_number = parts[0].strip()
+                company_social_denomination = parts[1].split('(')[0].strip()
+                # Modificamos la expresión regular para ser más flexible
+                company_name_match = re.search(r'(.*?)(?:\s+(SOCIEDAD LIMITADA|SL|SLL|SL EN LIQUIDACION|SLNE|SLNE EN LIQUIDACION|SA|SA EN LIQUIDACION|SOCIEDAD LIMITADA LABORAL|SOCIEDAD ANONIMA|S\.L\.|S\.L\.L\.|S\.A\.|S\.A\.A\.|S\.L\.L\.))?(?:\s*\(.+\))?\s*\.?$', company_social_denomination, re.IGNORECASE)
+                if company_name_match:
+                    company_name = company_name_match.group(1).strip()
+                else:
+                    company_name = 'Nombre no encontrado'
+                    
+                
+                  
+                
+
+                extracted_data.append((inscription_name, inscription_number, company_social_denomination, company_name))
+                
+    
+    return extracted_data
+
+   
 # -------------------------------------------------- PROCESAMIENTO BORME B --------------------------------------------------------------
 
 def file_type_b(pdf_path):
     companies = []
     
     texto_del_pdf = extract_text_from_pdf(pdf_path)
+    blocks = extract_blocks_with_titles(texto_del_pdf, pdf_path)
     
-    #print (texto_del_pdf)
+    
     
     
     # Extraer la fecha del Borme
@@ -62,7 +107,7 @@ def file_type_b(pdf_path):
      
     # Formatear la fecha al formato DD/MM/YYYY
      borme_date = f"{day.zfill(2)}/{month}/{year}"
-     print(borme_date)
+     
     else:
      borme_date = "Fecha no encontrada"
      
@@ -70,106 +115,22 @@ def file_type_b(pdf_path):
     inscription_commercial_registry_pattern = r'Empresarios\s+Otros actos publicados en el Registro Mercantil\s+([A-ZÁÉÍÓÚÜ\s]+)\n'
     inscription_commercial_registry_match = re.search(inscription_commercial_registry_pattern, texto_del_pdf)
     inscription_commercial_registry = inscription_commercial_registry_match.group(1) if inscription_commercial_registry_match else "No encontrado"
-    print(inscription_commercial_registry)
+    
     # Buscar Sección del Acto
     inscription_section_pattern = r'Pág\.\s+\d+\s+SECCIÓN\s+(PRIMERA|SEGUNDA)'
     inscription_section_match = re.search(inscription_section_pattern, texto_del_pdf)
     inscription_section = inscription_section_match.group(1) if inscription_section_match else "No encontrado"
-    print(inscription_section)
+  
     # Buscar Categoria del Acto
     inscription_category_pattern = r'Empresarios\s+(.*)\s+'
     inscription_category_match = re.search(inscription_category_pattern, texto_del_pdf)
     inscription_category = inscription_category_match.group(1) if inscription_category_match else "No encontrado"
-    print(inscription_category)
+   
     
-    # Buscar todos los actos inscritos y extraer informacion
-    inscription_pattern = r'(\d{5}) - ([^(\n]+) \((\d{4})\)'
-    inscriptions = re.findall(inscription_pattern, texto_del_pdf)
-    print(inscriptions)
-    
-    for inscription in inscriptions:
-    # Expresión regular para buscar el número de inscripción
-        inscription_number_pattern = r'/^\d+/m'
-        number_match = re.search(inscription_number_pattern, inscription)
-        if number_match:
-         inscription_number = number_match.group(1)
-        else:
-         inscription_number = 'Número no encontrado'
-            
-    # Expresión regular para buscar la denominación social completa
-        full_name_pattern = r'/^\d+\s-\s(.*)(\R|$)/m'
-        full_name_match = re.search(full_name_pattern, inscription, re.IGNORECASE)
-    
-        if full_name_match:
-         company_social_denomination = full_name_match.group(1).strip()
-        else:
-         company_social_denomination = 'Denominación no encontrada'
-    # Expresión regular para buscar solo el nombre de la empresa (sin SL, Sociedad Limitada, etc.)
-        name_pattern = r'^\d+ - (.*?)(?:,? (SOCIEDAD LIMITADA|SL|SLL|SL EN LIQUIDACION|SLNE|SLNE EN LIQUIDACION|SA|SA EN LIQUIDACION|SOCIEDAD LIMITADA LABORAL|SOCIEDAD ANONIMA|S\.L\.|S\.L\.L\.|S\.A\.|S\.A\.A\.|S\.L\.L\.))?\.\n'
-        name_match = re.search(name_pattern, inscription, re.IGNORECASE)
-    
-        if name_match:
-         company_name = name_match.group(1).strip()
-        else:
-         company_name = 'Nombre no encontrado' 
-
-    # Expresión regular para extraer la fecha
-        date_pattern = r'\(\s*(\d{1,2})\.(\d{1,2})\.(\d{2,4})\)'
-        date_match = re.search(date_pattern, inscription)
-
-        if date_match:
-         day, month, year = date_match.groups()
-    # Asegurar que el día y el mes tengan dos dígitos
-         day = day.zfill(2)
-         month = month.zfill(2)
-
-    # Ajustar el año para que tenga cuatro dígitos
-         year = "20" + year if len(year) == 2 else year
-
-    # Formatear la fecha al formato DD/MM/YYYY
-         inscription_date = f"{day}/{month}/{year}"
-        else:
-          inscription_date = 'Fecha no encontrada' 
-            
-    # Relaciona cada acto con las palabras en negrita de inscription        
-        inscription_name =  "italic_text"
-        
-
-       
-        company = {
-            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "companySocialDenomination": company_social_denomination,          
-            "companyName": company_name,
-            "companyNif": None,
-            "companyCurrentAddress": None,
-            "companyCurrentSocialObject": None,
-            "companyCurrentSocialCapital": None,
-            "companyPhoneNumber": None,
-            "companyEmail": None,
-            "companyWeb": None,
-            "companyEmployeesNumber": None,
-            "companyParent": None,
-            "constitutionCommercialRegistry": None,
-            "constitutionDate":  None,
-            "constitutionRegistryData": None,
-            "constitutionInscription": None,
-            "operationsStartDate": None,
-            "companyDuration": None,
-            "constitutionAddress":None,
-            "constitutionSocialObject": None,
-            "constitutionSocialCapital": None,
-            "constitutionFile": None,            
-            "companyState": None,
-            "administration": None,
-            "administratorsList": None,
-            "administrationAppointmentDate": None,
-            "administrationAppointmentInscription": None,
-            "administrationAppointmentFile": None,
-            "companyInscription": []
-        }
-        # Crear y agregar la inscripción a la empresa correspondiente
-        company_inscription  = {
+    for block in blocks:
+        processed_data = process_block(block)
+        for inscription_name, inscription_number, company_social_denomination, company_name in processed_data:
+            company_inscription = {
                 "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "inscriptionCommercialRegistry": inscription_commercial_registry,
@@ -177,27 +138,33 @@ def file_type_b(pdf_path):
                 "inscriptionSection": inscription_section,
                 "inscriptionCategory": inscription_category,
                 "inscriptionName": inscription_name,
-                "inscriptionDate": inscription_date,
+                #"inscriptionDate": inscription_date,
                 "inscriptionRegistryData": None,
-                "inscription": inscription,
+                "inscription": block,
                 "bormeDate": borme_date,
                 "inscriptionFile": os.path.basename(pdf_path)
-                }
-            
-        company["companyInscription"].append(company_inscription)
-        companies.append(company)
+            }
+
+            company = {
+                "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "companySocialDenomination": company_social_denomination,
+                "companyName": company_name,
+                "companyInscription": [company_inscription]
+            }
+
+            companies.append(company)
 
     return companies
 
 
 @app.route('/')  # Defino la ruta
 def home():
-    pdf_path = "files/pruebas chicas/prueba_B/2009/02/11/pdfs/BORME-B-2009-28-35.pdf"
+    pdf_path = "files/pruebas chicas/prueba_B/2009/02/11/pdfs/BORME-B-2009-28-48.pdf"
     texto_del_pdf = extract_text_from_pdf(pdf_path)    
     company = file_type_b(pdf_path)
     italic_titles = extract_italic_titles(pdf_path)
-    for title in italic_titles:
-     print(title)  
+    
     
     return jsonify(company)
 
